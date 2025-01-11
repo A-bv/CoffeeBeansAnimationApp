29aad259c8ec55ddca685994b6190c0e8b2e8b64
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2025-01-11 19:40:16 +0100

    Initial Commit

---
+++ CoffeeBeansAnimationApp/CoffeeBeansAnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBeansAnimationAppApp.swift
+//  CoffeeBeansAnimationApp
+//
+//  Created by Alexandre Bevilacqua on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBeansAnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeBeansAnimationAppTests/CoffeeBeansAnimationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBeansAnimationAppTests.swift
+//  CoffeeBeansAnimationAppTests
+//
+//  Created by Alexandre Bevilacqua on 11.01.2025.
+//
+
+import Testing
+@testable import CoffeeBeansAnimationApp
+
+struct CoffeeBeansAnimationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
